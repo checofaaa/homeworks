a5abf05eb3afda92dc069f1eca907ce64a76ddf9
sofya <scheurina@gmail.com>
2025-11-05 14:56:10 +0300

    "Изменено имя"

--- src/main/java/org/example/Lesson2.java
+++ src/main/java/org/example/Lesson2.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class lesson_2 {
+public class Lesson2 {
     public static void main(String[] args) {
         Product product1 = new Product(
                 "banana",
